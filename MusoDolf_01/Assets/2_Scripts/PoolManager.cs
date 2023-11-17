43fd85447752cea52686a7afa9266e9252c5e6bb
공익달프 <wjddnwls316@naver.com>
2023-11-17 10:42:31 +0900

    베이스
    맘 편하게 Stage할때 그냥 Asset Package만 올리기

---
+++ MusoDolf_01/Assets/2_Scripts/PoolManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PoolManager : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    List<GameObject>[] pools;
+
+    void Awake()
+    {
+        pools = new List<GameObject>[prefabs.Length];
+
+        for (int index = 0; index < pools.Length; index ++)
+        {
+            pools[index] = new List<GameObject>();
+        }
+    }
+
+    public GameObject Get(int index)
+    {
+        GameObject select = null;
+        // . . . 선택한 풀의 비활성화 되어 있는 게임오브젝트 접근
+            // . . . 발견하면 select 변수에 할당
+
+        foreach (GameObject item in pools[index])
+        {
+            if(!item.activeSelf)
+            {
+                select = item;
+                select.SetActive(true);
+                break;
+            }
+        }
+
+        if (!select)
+        {
+            select = Instantiate(prefabs[index], transform);
+            pools[index].Add(select);
+        }
+        // . . . 못찾았을 경우
+            // . . .새롭게 생성하고 select 변수에 할당
+        return select;
+    }
+}
