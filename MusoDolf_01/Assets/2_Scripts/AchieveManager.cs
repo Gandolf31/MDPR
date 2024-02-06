354a8a86a9a56c6c0444f3e09577da4e61b0a947
간달프 <wjddnwls316@naver.com>
2024-02-06 16:35:29 +0900

    알림 오브젝트 저장부터 시작
    PlayerPref

---
+++ MusoDolf_01/Assets/2_Scripts/AchieveManager.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchieveManager : MonoBehaviour
+{
+    public GameObject[] lockCharacter;
+    public GameObject[] unlockCharacter;
+
+    enum Achieve { UnlockPotato, UnlockBean };
+    Achieve[] achieves;
+
+    void Awake()
+    {
+        achieves = (Achieve[])Enum.GetValues(typeof(Achieve));
+
+        if(!PlayerPrefs.HasKey("MyData"))
+        {
+            Init();
+        }
+    }
+
+    void Init()
+    {
+        PlayerPrefs.SetInt("MyData", 1);
+        foreach(Achieve achieve in achieves)
+        {
+            PlayerPrefs.SetInt(achieve.ToString(), 0);
+        }
+    }
+
+    void Start()
+    {
+        UnlockCharacter();
+    }
+    void UnlockCharacter()
+    {
+        for(int index=0; index < lockCharacter.Length; index++)
+        {
+            string achieveName = achieves[index].ToString();
+            bool isUnlock = PlayerPrefs.GetInt(achieveName) == 1;
+            lockCharacter[index].SetActive(!isUnlock);
+            unlockCharacter[index].SetActive(isUnlock);
+        }
+    }
+
+    void LateUpdate()
+    {
+        foreach(Achieve achieve in achieves)
+        {
+            CheckAchieve(achieve);
+        }
+    }
+
+    void CheckAchieve(Achieve achieve)
+    {
+        bool isAchieve = false;
+
+        switch (achieve)
+        {
+            case Achieve.UnlockPotato:
+                isAchieve = GameManager.instance.kill >= 10;
+                break;
+            case Achieve.UnlockBean:
+                isAchieve = GameManager.instance.gameTime == GameManager.instance.maxGameTime;
+                break;
+        }
+
+        if(isAchieve && PlayerPrefs.GetInt(achieve.ToString()) == 0)
+        {
+            PlayerPrefs.SetInt(achieve.ToString(), 1);
+        }
+    }
+}
